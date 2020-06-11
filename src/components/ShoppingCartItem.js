a1f577d16e1395b00e678fbf6594287285db38be
jasonelliots <j.elliotschwartz@gmail.com>
2020-06-11 13:44:56 -0700

    finished MVP and remove button as per stretch

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import {CartContext} from '../contexts/CartContext'; 
 
 const Item = props => {
+
+	const {cart, setCart} = useContext(CartContext); 
+
+	//try putting this function in app component and passing it down 
+	const removeItem = () => {
+		setCart(cart.filter(item => {
+			if(item.id !== props.id){
+				return item 
+			} else {
+				return null
+			}
+		}))
+	}
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +25,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={removeItem}>Remove from cart</button>
 			</div>
 		</div>
 	);
